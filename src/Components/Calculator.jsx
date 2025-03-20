cfb18efc49a9c7e71ccf7305fe6c0afa4493cc9a
hunain soomro <huniiali1122@gmail.com>
2025-03-21 03:44:52 +0500

    first commit

---
+++ src/Components/Calculator.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Calculator = () => {
+  return (
+    <div>
+        <div className="main">
+        <h1>Calculator</h1>
+
+        </div>
+    </div>
+  )
+}
+
+export default Calculator
