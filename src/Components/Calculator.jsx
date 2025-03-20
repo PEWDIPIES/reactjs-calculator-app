b8dca95dad527cde830a2426c248353287ae14c4
hunain soomro <huniiali1122@gmail.com>
2025-03-21 04:13:38 +0500

    Initial commit with calculator code

--- src/Components/Calculator.jsx
+++ src/Components/Calculator.jsx
@@ -4,7 +4,34 @@ const Calculator = () => {
   return (
     <div>
         <div className="main">
-        <h1>Calculator</h1>
+        <div className="calculator">
+        <div className="display" id="display">1234</div>
+        <div className="buttons">
+            <button className="button gray" onclick="clearDisplay()">AC</button>
+            <button className="button gray">+/-</button>
+            <button className="button gray">%</button>
+            <button className="button orange">÷</button>
+            
+            <button className="button dark" onclick="updateDisplay('7')">7</button>
+            <button className="button dark" onclick="updateDisplay('8')">8</button>
+            <button className="button dark" onclick="updateDisplay('9')">9</button>
+            <button className="button orange">×</button>
+            
+            <button className="button dark" onclick="updateDisplay('4')">4</button>
+            <button className="button dark" onclick="updateDisplay('5')">5</button>
+            <button className="button dark" onclick="updateDisplay('6')">6</button>
+            <button className="button orange">-</button>
+            
+            <button className="button dark" onclick="updateDisplay('1')">1</button>
+            <button className="button dark" onclick="updateDisplay('2')">2</button>
+            <button className="button dark" onclick="updateDisplay('3')">3</button>
+            <button className="button orange">+</button>
+            
+            <button className="button dark" onclick="updateDisplay('0')">0</button>
+            <button className="button dark">.</button>
+            <button className="button dark" onclick="calculateResult()">=</button>
+        </div>
+    </div>
 
         </div>
     </div>
